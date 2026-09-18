959156336214b4ba0360aa632701a95157364747
shubham <shubhamrothe@users.noreply.github.com>
2026-09-18 10:03:51 +0530

    Make response submission cross-origin safe

--- script.js
+++ script.js
@@ -110,7 +110,7 @@ form.addEventListener('submit', async (event) => {
   try {
     await fetch(responseEndpoint, {
       method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+      headers: { 'Content-Type': 'text/plain;charset=utf-8' },
       body: JSON.stringify(response),
       mode: 'no-cors'
     });
